da2af08574c67e1dbf52fb9c4c20fe8848948cde
Jamie Mortensen <16mortensenj@gmail.com>
2024-01-21 17:09:10 -0500

    adding sliders

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,29 +1,39 @@
 $(document).ready(function () {
-    $.ajax({
-        url: '/update_graph',
-        type: 'POST',
-        data: JSON.stringify({
-            'years': 10,
-            'annual_medical_expense': 5000,
-            'hsa_return_rate': 0.07,
-            'tax_rate': 0.24,
-            'hsa_withdrawal_rate': 5000,
-            'hsa_contribution': 3550
-        }),
-        contentType: 'application/json; charset=utf-8',
-        dataType: 'json',
-        async: false,
-        success: function (data) {
-            var layout = {
-                title: 'HSA Analysis',
-                xaxis: {
-                    title: 'Year'
-                },
-                yaxis: {
-                    title: 'Amount ($)'
-                }
-            };
-            Plotly.newPlot('graph', data, layout);
-        }
+    function updatePlot() {
+        var years = $('#years').val();
+        var annual_medical_expense = $('#annual_medical_expense').val();
+        // get values of other sliders...
+
+        $.ajax({
+            url: '/update_graph',
+            type: 'POST',
+            data: JSON.stringify({
+                'years': years,
+                'annual_medical_expense': annual_medical_expense,
+                // other data...
+            }),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            async: false,
+            success: function (data) {
+                var layout = {
+                    title: 'HSA Analysis',
+                    xaxis: {
+                        title: 'Year'
+                    },
+                    yaxis: {
+                        title: 'Amount ($)'
+                    }
+                };
+                Plotly.newPlot('graph', data, layout);
+            }
+        });
+    }
+
+    $('#years, #annual_medical_expense').on('input', function () {
+        $('#' + this.id + '-value').text(this.value);
+        updatePlot();
     });
+
+    updatePlot();
 });
