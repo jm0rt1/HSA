9fa5bac1c3a4dff468d83e2b7a86ca956b0b3545
Jamie Mortensen <16mortensenj@gmail.com>
2024-01-21 17:04:41 -0500

    new js

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
     $.ajax({
         url: '/update_graph',
         type: 'POST',
-        data: {
+        data: JSON.stringify({
             'years': 10,
             'annual_medical_expense': 5000,
             'hsa_return_rate': 0.07,
             'tax_rate': 0.24,
             'hsa_withdrawal_rate': 5000,
             'hsa_contribution': 3550
-        },
+        }),
         contentType: 'application/json; charset=utf-8',
         dataType: 'json',
         async: false,
